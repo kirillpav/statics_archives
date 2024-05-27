f28097ca9a30b6bc76ddd66b50c4813892081ee9
kirill pavlov <2020kirillp@gmail.com>
2024-05-27 17:16:19 +0100

    Added basic animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
 import Image from "next/image";
+import { useRef } from "react";
 import styles from "../styles/hero.module.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+	const imgRef = useRef(null);
+
+	useGSAP(() => {
+		gsap.to(imgRef.current, {
+			duration: 1.5,
+			y: -20,
+			repeat: -1,
+			yoyo: true,
+			ease: "sine.inOut",
+			scrollTrigger: {
+				trigger: "#herosection",
+				start: "top bottom",
+				toggleActions: "play pause play pause",
+			},
+		});
+	});
+
 	return (
 		<>
 			<Image
@@ -10,6 +35,7 @@ export default function Home() {
 				height={500}
 				width={500}
 				className={styles.hero_gif}
+				ref={imgRef}
 			/>
 		</>
 	);
