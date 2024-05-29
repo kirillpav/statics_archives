ad6599a76634cfff13809d1c04f3a1b95151ffe4
kirill pavlov <2020kirillp@gmail.com>
2024-05-29 14:31:30 +0100

    UI Updates

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import styles from "../../styles/showcase.module.css";
+import { Select, SelectSection, SelectItem } from "@nextui-org/select";
 
 // image imports
 import heartbakeTiffany from "../../../public/imgs/heartbreak_tiffany.png";
@@ -16,38 +17,40 @@ import silver from "../../../public/imgs/60.1_silver.png";
 import blue from "../../../public/imgs/60.1_blue.png";
 import black from "../../../public/imgs/60.1_black.png";
 
+const collections: string[] = ["60.1 COLLECTION", "88.1 COLLECTION"];
+
 export default function showcase(): ReturnType<React.FC> {
 	return (
 		<>
 			<h3>60.1 COLLECTION</h3>
 			<div className={styles.showcase__container}>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image src={black} alt="first keyboard" height={400} width={300} />
 					<span>60.1 black</span>
 				</div>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image src={blue} alt="first keyboard" height={400} width={300} />
 					<span>60.1 blue</span>
 				</div>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image src={silver} alt="first keyboard" height={400} width={300} />
 					<span>60.1 silver</span>
 				</div>
 				<h3>88.1 COLLECTION</h3>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image src={alpine} alt="first keyboard" height={400} width={300} />
 					<span>88.1 alpine</span>
 				</div>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image src={lilac} alt="first keyboard" height={400} width={300} />
 					<span>88.1 lilac</span>
 				</div>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image src={sierra} alt="first keyboard" height={400} width={300} />
 					<span>88.1 sierra</span>
 				</div>
 				<h3>FOUNDATION COLLECTION</h3>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image
 						src={foundationNavy}
 						alt="first keyboard"
@@ -56,7 +59,7 @@ export default function showcase(): ReturnType<React.FC> {
 					/>
 					<span>foundaction navy</span>
 				</div>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image
 						src={foundationVeryPeri}
 						alt="first keyboard"
@@ -66,7 +69,7 @@ export default function showcase(): ReturnType<React.FC> {
 					<span>foundaction veryperi</span>
 				</div>
 				<h3>HEARTBAKE COLLECTION</h3>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image
 						src={heartbakeTiffany}
 						alt="first keyboard"
@@ -75,7 +78,7 @@ export default function showcase(): ReturnType<React.FC> {
 					/>
 					<span>heartbrake tiffany</span>
 				</div>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image
 						src={heartbrakeContemporaryBlack}
 						alt="first keyboard"
@@ -84,7 +87,7 @@ export default function showcase(): ReturnType<React.FC> {
 					/>
 					<span>heartbrake contemporary black</span>
 				</div>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image
 						src={heartbrakePurplePigeon}
 						alt="first keyboard"
@@ -93,7 +96,7 @@ export default function showcase(): ReturnType<React.FC> {
 					/>
 					<span>heartbrake purple pigeon</span>
 				</div>
-				<div>
+				<div className={styles.showcase__item}>
 					<Image
 						src={heartbrakeStrangeLove}
 						alt="first keyboard"
